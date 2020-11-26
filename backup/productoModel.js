b35f4134db4385541e10cf17c7e270b2b2f5a908
RDN <rdiegon@debianU100>
2020-11-26 18:50:47 +0000

    Comienzo api json.

--- backup/productoModel.js
+++
@@ -1,5 +0,0 @@
-//DATOS DE PRODUCTOS
-
-
-let data = [{ id: 1, nombre: "Heladera", precio: 50000, stock: 20 }];
-
--- backup/productoRoutes.js
+++
@@ -1,9 +0,0 @@
- 
-const express = require('express');
-const app = express.Router();
-const productoController = require('./productoController');
-
-
-app.get("/productos", productoController.getAllProducts );
-//app.get("/productos/:id", productoController.getOneProduct);
-module.exports = app;
